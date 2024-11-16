d56c745043ac3cd5e7cdee8fc3a5519bf11319f5
rhk998 <hemanthk.998r@gmail.com>
2024-11-16 14:49:40 +0530

    libray added getbooks

--- src/main/java/com/driver/services/BookService.java
+++ src/main/java/com/driver/services/BookService.java
@@ -4,6 +4,10 @@ import com.driver.models.Book;
 import com.driver.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
+
 
 import java.util.List;
 
@@ -21,6 +25,15 @@ public class BookService {
     public List<Book> getBooks(String genre, boolean available, String author){
         List<Book> books = null; //find the elements of the list by yourself
 
+        if (genre != null && author != null) {
+            return bookRepository2.findBooksByGenreAuthor(genre, author, available);
+        } else if (genre != null) {
+            return bookRepository2.findBooksByGenre(genre, available);
+        } else if (author != null) {
+            return bookRepository2.findBooksByAuthor(author, available);
+        } else {
+            return bookRepository2.findByAvailability(available);
+        }
         return books;
     }
 }
