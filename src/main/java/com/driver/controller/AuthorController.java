dc62736323617940ea30ee2b0636d179bee5dc01
rhk998 <hemanthk.998r@gmail.com>
2024-12-09 22:28:21 +0530

    cases

--- src/main/java/com/driver/controller/AuthorController.java
+++ src/main/java/com/driver/controller/AuthorController.java
@@ -2,6 +2,7 @@ package com.driver.controller;
 
 import com.driver.models.Author;
 import com.driver.repositories.AuthorRepository;
+import com.driver.services.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,11 +18,11 @@ public class AuthorController {
 
     //Write createAuthor API with required annotations
     @Autowired
-    AuthorRepository authorRepository;
+    AuthorService authorService;
 
     @PostMapping
     public ResponseEntity<String> Author(@RequestBody Author author){
-            authorRepository.addAuthor(author);
+            authorService.create(author);
             return new ResponseEntity<>("SUCCESS", HttpStatus.ACCEPTED);
     }
 }
